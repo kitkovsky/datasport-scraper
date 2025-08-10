7ed290ee1f505b60db2014dc8c3c0be2b5d94a57
kitkovsky <okitkowski114@gmail.com>
2025-08-10 19:55:58 +0200

    db connection: clean up unused vars

--- db/connection.py
+++ db/connection.py
@@ -9,8 +9,6 @@ from db.models import Base
 current_directory = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(f"{current_directory}/.env")
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
