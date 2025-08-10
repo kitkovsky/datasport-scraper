06ecbc3aefad328b6e90f34827611cf8d792485c
kitkovsky <okitkowski114@gmail.com>
2025-08-10 18:44:51 +0200

    use a socket connection to connect to the db

--- db/connection.py
+++ db/connection.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import Session
 
 from db.models import Base
@@ -15,10 +16,20 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 ENVIRONMENT = os.getenv("ENVIRONMENT")
+CLOUD_SQL_CONNECTION_NAME = os.getenv("CLOUD_SQL_CONNECTION_NAME")
 
 db_url = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(db_url, echo=ENVIRONMENT == "development")
+engine = create_engine(
+    URL.create(
+        drivername="postgresql+psycopg2",
+        username=DB_USER,
+        password=DB_PASSWORD,
+        query={
+            "unix_sock": "/cloudsql/{}/.s.PGSQL.5432".format(CLOUD_SQL_CONNECTION_NAME)
+        },
+    )
+)
 Base.metadata.create_all(bind=engine)
 
 session = Session(engine)
